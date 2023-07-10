0f3894ccc07ad97683d2532a36672df2946f6a0a
LouisSyrepret <51822107+LouisSyrepret@users.noreply.github.com>
2023-07-10 17:21:35 +0200

    Update scanner.js (parenthesis error)

--- scanner.js
+++ scanner.js
@@ -4,7 +4,7 @@ function extraireInfo(xmlDoc) {
   let printStr = '<b>Farmhands</b> : ';
   if(farmhands.length == 0) { printStr += '<i>none</i>'; }
   for(let i = 0; i<farmhands.length; i++) {
-    printStr += ' '+farmhands[i].getElementsByTagName('name')[0].innerText)+(i+1 == farmhands.length ? '' : ',');
+    printStr += ' '+farmhands[i].getElementsByTagName('name')[0].innerText+(i+1 == farmhands.length ? '' : ',');
   }
   printStr += '<br>';
   return {print:printStr};
