ed68cd8291dd53d3751312ee96a6b03bfd638579
LouisSyrepret <51822107+LouisSyrepret@users.noreply.github.com>
2023-07-10 17:31:14 +0200

    Update scanner.js

--- scanner.js
+++ scanner.js
@@ -5,7 +5,7 @@ function extraireInfo(xmlDoc) {
   let printStr = '<b>Farmhands</b> : ';
   if(farmhands.length == 0) { printStr += '<i>none</i>'; }
   for(let i = 0; i<farmhands.length; i++) {
-    printStr += ' '+farmhands[i].getElementsByTagName('name')[0].innerText+(i+1 == farmhands.length ? '' : ',');
+    printStr += ' '+farmhands[i].getElementsByTagName('name')[0].innerHTML+(i+1 == farmhands.length ? '' : ',');
   }
   printStr += '<br>';
   return {printversion:printStr};
