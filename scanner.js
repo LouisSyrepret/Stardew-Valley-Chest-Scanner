07709566a0e08441cbb086b5f51293e83552444e
LouisSyrepret <51822107+LouisSyrepret@users.noreply.github.com>
2023-07-10 18:55:29 +0200

    Update scanner.js (calcul exact du obtenirChemin)

--- scanner.js
+++ scanner.js
@@ -1,10 +1,22 @@
-function obtenirXChemin(xmlDoc,chemin) {
-  return xmlDoc.document.evaluate(chemin, xmlDoc, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+function obtenirChemin(xmlDoc,chemin) {
+  let sections = chemin.split('/');
+  let NODE = xmlDoc;
+  for(let s = 0; s<sections.length; s++) {
+    let sect = sections[s]; let nom = sect; let num = 0;
+    if(sect.indexOf('[') != -1) {
+        nom = sect.slice(0,sect.indexOf('['));
+        num = sect.slice(sect.indexOf('[')+1, sect.indexOf(']'));
+    }
+    let possibilites = NODE.getElementsByTagName(nom);
+    if(num >= possibilites.length) { return null; }
+    NODE = NODE.getElementsByTagName(nom)[num];
+  }
+  return NODE;
 }
 
 function obtenirParametre(xmlDoc,code) {
-  let node = xmlDoc.document.evaluate(chemin, xmlDoc, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-  if(node == undefined) {
+  let node = obtenirChemin(xmlDoc,code);
+  if(node == null) {
     return '';
   } else {
     return node.innerHTML;
@@ -18,10 +30,10 @@ function etoile(qualite) {
 // Ce bloc de code a pour but de récupérer le contenu de tous les inventaires et les coffres.
 function extraireInfo(xmlDoc) {
   let printStr = '<table><thead><tr><th>Élément</th><th>Quantité</th><th>Commentaire</th></tr></thead><tbody>';
-  let inventaireJoueur = obtenirXChemin('/SaveGame/player/items');
+  let inventaireJoueur = obtenirChemin('SaveGame/player/items');
   for(let i = 0; i<inventaireJoueur.childElementCount; i++) {
     let item = inventaireJoueur.children[i];
-    printStr += '<tr><td>'+obtenirParametre(item,'/name')+'</td><td>'+obtenirParametre(item,'/Stack')+'</td><td>'+obtenirParametre(item,'/type')+'</td></tr>';
+    printStr += '<tr><td>'+obtenirParametre(item,'name')+'</td><td>'+obtenirParametre(item,'Stack')+'</td><td>'+obtenirParametre(item,'type')+'</td></tr>';
   }
   printStr += '</tbody></table>';
   return {printversion:printStr};
